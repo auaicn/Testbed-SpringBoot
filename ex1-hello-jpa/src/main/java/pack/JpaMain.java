ec21027fcc1306a2cf95b81df0757fdddd559468
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-07-11 15:36:55 +0900

    Google CodeStyle 적용 + Column Field 매핑
    @Enumerated
    @Column
    @Temporal
    @Lob

--- ex1-hello-jpa/src/main/java/pack/JpaMain.java
+++ ex1-hello-jpa/src/main/java/pack/JpaMain.java
@@ -27,68 +27,69 @@ public class JpaMain {
         try {
             for (int i=0;i<20;i++){
                 Member newMember = new Member();
-                newMember.setName("auaicn " + i);
+                newMember.setName("auaicn " + i); // not null field 에 대해서 무조건 해줘야 한다.
                 newMember.setId((long)i);
+                newMember.setRoleType(RoleType.ADMINISTRATOR);
                 em.persist(newMember);
             }
-
-            // 그런데, 18 살 이상의 모든 회원들을 가져오는 등 고급 검색(통계) 기능을 사용하고 싶다면 JPQL 을 사용해야한다
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(10)
-                    .setMaxResults(10)
-                    .getResultList();
-
-            for (Member mem: result) {
-                // System.out.println("menber.name = " + mem.getName());
-            }
-
-            em.clear();
-
-            System.out.println("\n========= level-1 caching Test =========\n");
-
-            System.out.println("\n5th, before caching");
-            Member _5thMember = em.find(Member.class,5L);
-
-            // 1차 caching 확인
-            System.out.println("\n5th(same), maybe cached");
-            Member _5again = em.find(Member.class,5L);
-
-            /* detach test 는 persistence.xml 의 hbm2ddl.auto option 을 create 로 해놔서 확인이 어려움 */
-            System.out.println("\n========= Dirty Check Test =========\n");
-
-            // 6번째 녀석 찾아와놓고 시작
-            Member _6thMember = em.find(Member.class,6L);
-            System.out.println("6번째 녀석 찾아와놓고 시작\n");
-
-            // dirty check (clean case)
-            System.out.println("6번째 녀석, dirty check (clean case)");
-            em.persist(_6thMember);
-            em.flush();
-
-            // dirty check (dirty case)
-            System.out.println("6번째 녀석, dirty check (dirty case)");
-            _6thMember.setName(_6thMember.getName() + " i'm dirty");
-            em.persist(_6thMember);
-            em.flush();
-
-
-            System.out.println("\n========= Persistence / Detach Test =========\n");
-            Member persistentMember = em.find(Member.class,11L);
-            Member detachedMember = em.find(Member.class,12L);
-            System.out.println("11번째 : 영속 / 12번째 : 준영속 찾아놓고 시작");
-
-            // 영속 상태
-            System.out.println("\n영속 상태, 11번 녀석");
-            persistentMember.setName("not detached. so changed!");
-            em.flush();
-
-            // 준영속 상태
-            System.out.println("\n준영속 상태, 12번 녀석");
-            em.detach(detachedMember);
-            detachedMember.setName("not detached. so changed!");
-            em.flush();
-
-            System.out.println("\n======== now commit ========\n");
+//
+//            // 그런데, 18 살 이상의 모든 회원들을 가져오는 등 고급 검색(통계) 기능을 사용하고 싶다면 JPQL 을 사용해야한다
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(10)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            for (Member mem: result) {
+//                // System.out.println("menber.name = " + mem.getName());
+//            }
+//
+//            em.clear();
+//
+//            System.out.println("\n========= level-1 caching Test =========\n");
+//
+//            System.out.println("\n5th, before caching");
+//            Member _5thMember = em.find(Member.class,5L);
+//
+//            // 1차 caching 확인
+//            System.out.println("\n5th(same), maybe cached");
+//            Member _5again = em.find(Member.class,5L);
+//
+//            /* detach test 는 persistence.xml 의 hbm2ddl.auto option 을 create 로 해놔서 확인이 어려움 */
+//            System.out.println("\n========= Dirty Check Test =========\n");
+//
+//            // 6번째 녀석 찾아와놓고 시작
+//            Member _6thMember = em.find(Member.class,6L);
+//            System.out.println("6번째 녀석 찾아와놓고 시작\n");
+//
+//            // dirty check (clean case)
+//            System.out.println("6번째 녀석, dirty check (clean case)");
+//            em.persist(_6thMember);
+//            em.flush();
+//
+//            // dirty check (dirty case)
+//            System.out.println("6번째 녀석, dirty check (dirty case)");
+//            _6thMember.setName(_6thMember.getName() + " i'm dirty");
+//            em.persist(_6thMember);
+//            em.flush();
+//
+//
+//            System.out.println("\n========= Persistence / Detach Test =========\n");
+//            Member persistentMember = em.find(Member.class,11L);
+//            Member detachedMember = em.find(Member.class,12L);
+//            System.out.println("11번째 : 영속 / 12번째 : 준영속 찾아놓고 시작");
+//
+//            // 영속 상태
+//            System.out.println("\n영속 상태, 11번 녀석");
+//            persistentMember.setName("not detached. so changed!");
+//            em.flush();
+//
+//            // 준영속 상태
+//            System.out.println("\n준영속 상태, 12번 녀석");
+//            em.detach(detachedMember);
+//            detachedMember.setName("not detached. so changed!");
+//            em.flush();
+//
+//            System.out.println("\n======== now commit ========\n");
 
             tx.commit();
         }catch (Exception e){
--- ex1-hello-jpa/src/main/java/pack/Member.java
+++ ex1-hello-jpa/src/main/java/pack/Member.java
@@ -1,9 +1,15 @@
 package pack;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 public class Member {
@@ -11,8 +17,24 @@ public class Member {
     @Id
     private Long id;
 
-    @Column (length = 22, unique = true)
+    @Column(length = 22, unique = true, nullable = false)
     private String name;
+
+    //    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.ORDINAL)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
 
     public Long getId() {
         return id;
@@ -28,5 +50,49 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Member() {
+
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
     }
 }
