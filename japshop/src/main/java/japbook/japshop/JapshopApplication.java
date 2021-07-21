820cc79aeae228c7b156c988a5fb5eef44f35ac5
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-07-21 16:08:02 +0900

    JPA SpringBoot 활용 1편 시작

---
+++ japshop/src/main/java/japbook/japshop/JapshopApplication.java
@@ -0,0 +1,13 @@
+package japbook.japshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapshopApplication.class, args);
+	}
+
+}
