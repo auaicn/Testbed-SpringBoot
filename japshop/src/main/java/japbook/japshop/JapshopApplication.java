930edb1d2659000305fe1a95189b79dbf28e1ba3
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-07-21 16:40:08 +0900

    [Lombok] Practice
    - `Application` 시작 부분(main) 은, 루프를 돌지 않는다.
    - 실험결과, 세팅에서 `Annotation Processor enabled` 는 굳이 켜줄필요 없는 것 같다. 이미 library loading 되어있는데, 어떤 추가설정인지 잘 모르겠다.
    - `build.gradle` 에서 계속해서 `sourceCompatibility = 11` 이 적혀있는데, 강의의 `1.8` 과는 다르게 설저되는것 같다. 문제가 있을수도 있을것 같다.

--- japshop/src/main/java/japbook/japshop/JapshopApplication.java
+++ japshop/src/main/java/japbook/japshop/JapshopApplication.java
@@ -7,7 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JapshopApplication {
 
 	public static void main(String[] args) {
+
+		// Application 이 돌아가도록하고, iOS 처럼 루프를 도는것이 아니라, 뒤의 작성줄들도 실행이 된다.
 		SpringApplication.run(JapshopApplication.class, args);
+
+		// Lombok Example
+		// 설명과는 다르게 IDE 상의 Annotation processor 는 설정을 꼭 해줄 필요는 없는것 같다.
+		LombokExample lombokExample = new LombokExample();
+		lombokExample.setData("my data");
+		System.out.println("data : " + lombokExample.getData());
+
 	}
 
 }
---
+++ japshop/src/main/java/japbook/japshop/LombokExample.java
@@ -0,0 +1,10 @@
+package japbook.japshop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LombokExample {
+
+    private String data;
+}
