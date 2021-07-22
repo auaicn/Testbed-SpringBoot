eef4a0f26b2cb2f93da0b45c5634d4fd3fd98eb7
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-07-22 12:20:54 +0900

    hihi

---
+++ japshop/src/test/java/japbook/japshop/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package japbook.japshop;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        // Assertions.assertThat(findMember.getId().equals((member.getId())));
+    }
+}
